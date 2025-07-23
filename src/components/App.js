0a1059e9ddbddd190a7ed8d5f11c02c67d928659
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-07-23 20:46:35 +0300

    Completed React App Example assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { format } from "date-fns";
+import { format } from "date-fns"; // this is fine to keep
 import ExampleComponent from "./ExampleComponent";
+import TestComponent from "./TestComponent"; // ✅ Don't forget to import it!
 
-// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
-      <p className="App-intro">
-        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
-        syntax. JSX lets us include JavaScript functions right along with the
-        HTML, and also allows us to add in components, which are separate,
-        self-contained chunks of JSX.
-      </p>
-      <ExampleComponent />
+      <h1>Now</h1> 
+      <ExampleComponent /> 
+      <TestComponent /> 
     </div>
   );
 }
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,18 +1,9 @@
 import React from "react";
 
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
 function TestComponent() {
   return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
+    <div title="time video">
+      <p>This is the test component!</p>
     </div>
   );
 }
